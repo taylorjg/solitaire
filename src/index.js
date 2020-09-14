6e28a4b31f0967cea37b04a4bb787d1f7cfba2d6
taylorjg <jonathan.taylor.programmer@gmail.com>
2020-09-14 23:23:54 +0100

    Draw pieces, toggle piece selection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { Solitaire } from './solitaire.js'
 
 const BOARD_SIZE = 400
+const BOARD_POSITION_RADIUS = BOARD_SIZE / 100
+const BOARD_PIECE_RADIUS = BOARD_SIZE / 40
+
+let selectedPiece = undefined
 
 const createSvgElement = (elementName, cssClass, attributes = {}) => {
   const element = document.createElementNS('http://www.w3.org/2000/svg', elementName)
@@ -11,23 +15,92 @@ const createSvgElement = (elementName, cssClass, attributes = {}) => {
 }
 
 const drawBoardPositions = (boardElement, boardPositions) => {
-  for (const [row, col] of Object.values(boardPositions)) {
+  for (const [row, col] of boardPositions) {
     const attributes = {
       cx: BOARD_SIZE / 8 * (col + 1),
       cy: BOARD_SIZE / 8 * (row + 1),
-      r: BOARD_SIZE / 80
+      r: BOARD_POSITION_RADIUS
     }
     const boardPositionElement = createSvgElement('circle', 'board-position', attributes)
     boardElement.appendChild(boardPositionElement)
   }
 }
 
+const drawBoardPieces = (boardElement, boardState) => {
+  const boardPieceElements = boardElement.querySelectorAll('.board-piece')
+  for (const boardPieceElement of boardPieceElements) {
+    boardElement.removeChild(boardPieceElement)
+  }
+  for (const [key, value] of Object.entries(boardState)) {
+    if (!value) {
+      continue
+    }
+    const [row, col] = Solitaire.keyToBoardPosition(key)
+    const attributes = {
+      cx: BOARD_SIZE / 8 * (col + 1),
+      cy: BOARD_SIZE / 8 * (row + 1),
+      r: BOARD_PIECE_RADIUS
+    }
+    const boardPieceElement = createSvgElement('circle', 'board-piece', attributes)
+    boardPieceElement.dataset.key = key
+    boardElement.appendChild(boardPieceElement)
+  }
+}
+
+const findBoardPieceElement = (boardElement, boardPosition) => {
+  const key = Solitaire.boardPositionToKey(boardPosition)
+  const boardPieceElements = boardElement.querySelectorAll('.board-piece')
+  for (const boardPieceElement of boardPieceElements) {
+    if (boardPieceElement.dataset.key === key) {
+      return boardPieceElement
+    }
+  }
+  return undefined
+}
+
+const boardClickToBoardPosition = (boardPositions, offsetX, offsetY) => {
+  for (const [row, col] of boardPositions) {
+    const rowY = BOARD_SIZE / 8 * (row + 1)
+    const colX = BOARD_SIZE / 8 * (col + 1)
+    const dx = Math.abs(colX - offsetX)
+    const dy = Math.abs(rowY - offsetY)
+    if (dx <= BOARD_PIECE_RADIUS && dy <= BOARD_PIECE_RADIUS) {
+      return [row, col]
+    }
+  }
+  return undefined
+}
+
+const sameBoardPosition = (pos1, pos2) =>
+  pos1[0] === pos2[0] && pos1[1] === pos2[1]
+
+const onBoardClick = (boardElement, boardPositions) => ({ offsetX, offsetY }) => {
+  const boardPosition = boardClickToBoardPosition(boardPositions, offsetX, offsetY)
+  if (!boardPosition) {
+    return
+  }
+  const boardPieceElement = findBoardPieceElement(boardElement, boardPosition)
+  if (selectedPiece) {
+    if (sameBoardPosition(selectedPiece, boardPosition)) {
+      selectedPiece = undefined
+      boardPieceElement.classList.remove('board-piece--selected')
+    } else {
+      // TODO: make move if valid
+    }
+  } else {
+    selectedPiece = boardPosition
+    boardPieceElement.classList.add('board-piece--selected')
+  }
+}
+
 const main = () => {
+  const solitaire = new Solitaire()
   const boardElement = document.querySelector('.board')
+  boardElement.addEventListener('click', onBoardClick(boardElement, solitaire.boardPositions))
   boardElement.style.width = BOARD_SIZE
   boardElement.style.height = BOARD_SIZE
-  const solitaire = new Solitaire()
   drawBoardPositions(boardElement, solitaire.boardPositions)
+  drawBoardPieces(boardElement, solitaire.boardState)
 }
 
 main()
--- src/solitaire.js
+++ src/solitaire.js
@@ -1,10 +1,47 @@
 import * as U from './utils.js'
 
+const CENTRE_X = 3
+const CENTRE_Y = 3
+
+const UP = 0
+const DOWN = 1
+const LEFT = 2
+const RIGHT = 3
+
+const DIRECTIONS = [UP, DOWN, LEFT, RIGHT]
+
 export class Solitaire {
   // validMoves => [actionIndex]
   // makeMove(actionIndex)
 
   constructor() {
+    this._boardPositions = Solitaire.createBoardPositons()
+    this._boardState = {}
+    this._actions = Solitaire.allPossibleActions(this._boardPositions)
+    this.reset()
+  }
+
+  get boardPositions() {
+    return this._boardPositions
+  }
+
+  get boardState() {
+    return this._boardState
+  }
+
+  reset() {
+    for (const boardPosition of this.boardPositions) {
+      const key = Solitaire.boardPositionToKey(boardPosition)
+      this._boardState[key] = true
+    }
+    const keyCentre = Solitaire.boardPositionToKey([CENTRE_X, CENTRE_Y])
+    this._boardState[keyCentre] = false
+  }
+
+  static boardPositionToKey = ([row, col]) => `${row}-${col}`
+  static keyToBoardPosition = key => key.split('-').map(Number)
+
+  static createBoardPositons = () => {
     const colRanges = [
       U.range(2, 5),
       U.range(2, 5),
@@ -14,19 +51,16 @@ export class Solitaire {
       U.range(2, 5),
       U.range(2, 5)
     ]
-    this._boardPositions = []
+    const boardPositions = []
     colRanges.forEach((colRange, row) => {
       for (const col of colRange) {
-        const boardPosition = [row, col]
-        const key = Solitaire.makeBoardPositionKey(boardPosition)
-        this._boardPositions[key] = boardPosition
+        boardPositions.push([row, col])
       }
     })
+    return boardPositions
   }
 
-  static makeBoardPositionKey = ([row, col]) => `${row}-${col}`
-
-  get boardPositions() {
-    return this._boardPositions
+  static allPossibleActions = boardPositions => {
+    return []
   }
 }
