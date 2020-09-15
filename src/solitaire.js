563daf85944f82ef7bcb39ccf1239bfb3669ab57
taylorjg <jonathan.taylor.programmer@gmail.com>
2020-09-15 10:17:58 +0100

    A little refactoring (2)

--- src/solitaire.js
+++ src/solitaire.js
@@ -8,25 +8,79 @@ const DOWN = 1
 const LEFT = 2
 const RIGHT = 3
 
+function* boardPositionGenerator() {
+  const colRanges = [
+    U.range(2, 5),
+    U.range(2, 5),
+    U.range(7),
+    U.range(7),
+    U.range(7),
+    U.range(2, 5),
+    U.range(2, 5)
+  ]
+  let row = 0
+  for (const colRange of colRanges) {
+    for (const col of colRange) {
+      yield [row, col]
+    }
+    row += 1
+  }
+}
+
+// function* allPossibleActionsGenerator(boardPositions) {
+
+//   const isValidBoardPosition = boardPosition => {
+//     const index = boardPositions.findIndex(item => Solitaire.sameBoardPosition(item, boardPosition))
+//     return index >= 0
+//   }
+
+//   const followDirection = (boardPosition, direction) => {
+//     const [row, col] = boardPosition
+//     switch (direction) {
+//       case UP: return [row - 1, col]
+//       case DOWN: return [row + 1, col]
+//       case LEFT: return [row, col - 1]
+//       case RIGHT: return [row, col + 1]
+//     }
+//   }
+
+//   for (const from of boardPositions) {
+//     for (const direction of [UP, DOWN, LEFT, RIGHT]) {
+//       const via = followDirection(from, direction)
+//       const to = followDirection(via, direction)
+//       if (isValidBoardPosition(from) &&
+//         isValidBoardPosition(via) &&
+//         isValidBoardPosition(to)) {
+//         const keyFrom = Solitaire.boardPositionToKey(from)
+//         const keyVia = Solitaire.boardPositionToKey(via)
+//         const keyTo = Solitaire.boardPositionToKey(to)
+//         yield { from, via, to, keyFrom, keyVia, keyTo }
+//       }
+//     }
+//   }
+// }
+
+const BOARD_POSITIONS = Array.from(boardPositionGenerator())
+// const ALL_POSSIBLE_ACTIONS = Array.from(allPossibleActionsGenerator(BOARD_POSITIONS))
+
 export class Solitaire {
 
   constructor() {
-    this._boardPositions = Solitaire.createBoardPositons()
+    this._actions = Solitaire.allPossibleActions(BOARD_POSITIONS)
     this._boardState = {}
-    this._actions = Solitaire.allPossibleActions(this._boardPositions)
     this.reset()
   }
 
   get boardPositions() {
-    return this._boardPositions
-  }
-
-  get boardState() {
-    return this._boardState
+    return BOARD_POSITIONS
   }
 
   get actions() {
     return this._actions
+  }
+
+  get boardState() {
+    return this._boardState
   }
 
   get validActions() {
@@ -42,25 +96,15 @@ export class Solitaire {
   }
 
   isValidMove = (from, to) =>
-    Boolean(this.findAction(from, to))
+    Boolean(this._findAction(from, to))
 
   makeMove = (from, to) => {
-    const action = this.findAction(from, to)
+    const action = this._findAction(from, to)
     if (action) {
       this._boardState[action.keyFrom] = false
       this._boardState[action.keyVia] = false
       this._boardState[action.keyTo] = true
     }
-  }
-
-  findAction = (from, to) => {
-    for (const action of this.validActions) {
-      if (Solitaire.sameBoardPosition(action.from, from) &&
-        Solitaire.sameBoardPosition(action.to, to)) {
-        return action
-      }
-    }
-    return undefined
   }
 
   reset = () => {
@@ -72,36 +116,45 @@ export class Solitaire {
     this._boardState[keyCentre] = false
   }
 
-  static boardPositionToKey = ([row, col]) => `${row}-${col}`
-  static keyToBoardPosition = key => key.split('-').map(Number)
-
-  static createBoardPositons = () => {
-    const colRanges = [
-      U.range(2, 5),
-      U.range(2, 5),
-      U.range(7),
-      U.range(7),
-      U.range(7),
-      U.range(2, 5),
-      U.range(2, 5)
-    ]
-    const boardPositions = []
-    colRanges.forEach((colRange, row) => {
-      for (const col of colRange) {
-        boardPositions.push([row, col])
+  _findAction = (from, to) => {
+    for (const action of this.validActions) {
+      if (Solitaire.sameBoardPosition(action.from, from) &&
+        Solitaire.sameBoardPosition(action.to, to)) {
+        return action
       }
-    })
-    return boardPositions
+    }
+    return undefined
   }
 
+  static boardPositionToKey = ([row, col]) => `${row}-${col}`
+  static keyToBoardPosition = key => key.split('-').map(Number)
+  static sameBoardPosition = (pos1, pos2) => pos1[0] === pos2[0] && pos1[1] === pos2[1]
+
   static allPossibleActions = boardPositions => {
+
+    const isValidBoardPosition = boardPosition => {
+      const index = boardPositions.findIndex(item => Solitaire.sameBoardPosition(item, boardPosition))
+      return index >= 0
+    }
+
+    const followDirection = (boardPosition, direction) => {
+      const [row, col] = boardPosition
+      switch (direction) {
+        case UP: return [row - 1, col]
+        case DOWN: return [row + 1, col]
+        case LEFT: return [row, col - 1]
+        case RIGHT: return [row, col + 1]
+      }
+    }
+
     const actions = []
     for (const from of boardPositions) {
       for (const direction of [UP, DOWN, LEFT, RIGHT]) {
-        const via = Solitaire.followDirection(from, direction)
-        const to = Solitaire.followDirection(via, direction)
-        if (Solitaire.isValidBoardPosition(boardPositions, via) &&
-          Solitaire.isValidBoardPosition(boardPositions, to)) {
+        const via = followDirection(from, direction)
+        const to = followDirection(via, direction)
+        if (isValidBoardPosition(from) &&
+          isValidBoardPosition(via) &&
+          isValidBoardPosition(to)) {
           const keyFrom = Solitaire.boardPositionToKey(from)
           const keyVia = Solitaire.boardPositionToKey(via)
           const keyTo = Solitaire.boardPositionToKey(to)
@@ -111,22 +164,4 @@ export class Solitaire {
     }
     return actions
   }
-
-  static followDirection = (boardPosition, direction) => {
-    const [row, col] = boardPosition
-    switch (direction) {
-      case UP: return [row - 1, col]
-      case DOWN: return [row + 1, col]
-      case LEFT: return [row, col - 1]
-      case RIGHT: return [row, col + 1]
-    }
-  }
-
-  static isValidBoardPosition = (boardPositions, boardPosition) => {
-    const index = boardPositions.findIndex(item => Solitaire.sameBoardPosition(item, boardPosition))
-    return index >= 0
-  }
-
-  static sameBoardPosition = (pos1, pos2) =>
-    pos1[0] === pos2[0] && pos1[1] === pos2[1]
 }
--- test/solitaire.spec.js
+++ test/solitaire.spec.js
@@ -4,13 +4,44 @@ const expect = chai.expect
 
 describe('Solitaire class', () => {
 
+  it('should have the expected number of board positions', () => {
+    const solitaire = new Solitaire()
+    expect(solitaire.boardPositions).to.have.lengthOf(33)
+  })
+
   it('should have the expected number of possible actions', () => {
     const solitaire = new Solitaire()
     expect(solitaire.actions).to.have.lengthOf(76)
+  })
+
+  it('should have the expected number of initial pieces', () => {
+    const solitaire = new Solitaire()
+    const occupiedKvps = Object.entries(solitaire.boardState).filter(([, value]) => value)
+    const unoccupiedKvps = Object.entries(solitaire.boardState).filter(([, value]) => !value)
+    expect(occupiedKvps).to.have.lengthOf(32)
+    expect(unoccupiedKvps).to.have.lengthOf(1)
+    const unoccupiedBoardPositionKey = unoccupiedKvps[0][0]
+    expect(unoccupiedBoardPositionKey).to.equal('3-3')
   })
 
   it('should have the expected number of initial valid actions', () => {
     const solitaire = new Solitaire()
     expect(solitaire.validActions).to.have.lengthOf(4)
   })
+
+  it('should successfully convert board position to key', () => {
+    const boardPosition = [2, 6]
+    const key = Solitaire.boardPositionToKey(boardPosition)
+    expect(key).to.equal('2-6')
+  })
+
+  it('should successfully convert key to board position', () => {
+    const key = '5-1'
+    const boardPosition = Solitaire.keyToBoardPosition(key)
+    expect(boardPosition).to.deep.equal([5, 1])
+  })
+
+  // TODO:
+  // makeMove()
+  // reset()
 })
